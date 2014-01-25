517550ecb510b4fa4098adffedcb8ec115a1349e
John <kohler.j@husky.neu.edut>
2014-01-24 22:36:00 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HuskyGamify::Application.config.secret_key_base = '30a15f5e1ed7fd33fdbb2660210823ccc6823335e7b2d9fcc05e500cca288b6c4f7618824fc66b31de092136ea459616199fccd20dc73756a8bd01b7cc584153'
